ba814e54b5b42aaa06aa98f43fdf80f809b29fba
kanefron5 <kanerfon5@gmail.com>
2015-04-08 19:35:28 +0300

    лэйаут сообщенек

--- app/src/main/java/com/kanefron5/vktest/Messages.java
+++ app/src/main/java/com/kanefron5/vktest/Messages.java
@@ -2,6 +2,8 @@ package com.kanefron5.vktest;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Роман on 08.04.2015.
@@ -11,7 +13,24 @@ public class Messages extends ActionBarActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         {
-            setContentView(R.layout.dd);
+            setContentView(R.layout.dialogs);
+            ListView lv = (ListView) findViewById(R.id.list1);
+            final String[] nav = {
+                    getResources().getString(R.string.Mes),
+                    getResources().getString(R.string.Mes),
+                    getResources().getString(R.string.Mes),
+                    getResources().getString(R.string.Mes),
+                    getResources().getString(R.string.Mes),
+                    getResources().getString(R.string.Mes),
+                    getResources().getString(R.string.Mes),
+                    getResources().getString(R.string.Mes),
+                    getResources().getString(R.string.Mes),
+                    getResources().getString(R.string.Mes),
+
+            };
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.lst, nav);
+            lv.setAdapter(adapter);
         }
     }
 }
